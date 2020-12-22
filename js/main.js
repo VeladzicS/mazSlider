786b049de06c755cb84affba7dc98674696b6198
VeladzicS <veladzicsenahid@gmail.com>
2020-12-22 06:17:06 +0100

    made changes to main.js, one function call for moving slider left to right instead of using two, less code, cleaner look

--- js/main.js
+++ js/main.js
@@ -29,90 +29,63 @@ $(document).ready(function ($) {
 
   //Glavna logika slidera je obrunta jer je u CSS za oba slidera dodan style "direction: rtl;"
 
-  const moveSliderToNext = function (
+  const moveSliderToNextOrPrevious = function (
     sliderBox,
     sliderArray,
     firstElement,
-    lastElement
+    lastElement,
+    whichButtonIsClicked
   ) {
     const firstImageWidth = $(sliderArray).first().children("img").outerWidth();
     const lastImageWidth = $(sliderArray).last().children("img").outerWidth();
 
-    const moveSliderVar = firstImageWidth * -1;
+    if (whichButtonIsClicked === "next") {
+      let moveSliderVar = firstImageWidth * -1;
+      // varijabala koja služi da se slider pomjera u lijevo za velicinu slike koja dolazi
+      let moveSliderToRightOrLeft =
+        parseInt($(sliderBox).css("left")) - lastImageWidth;
+      $(sliderBox).animate({ left: moveSliderToRightOrLeft }, 400, function () {
+        //pomjera prvu sliku u lijevo, a zadnju sliku u slideru na prvo mjesto (obrnuto zbog CCS "direction:rtl;")
+        $(firstElement).before($(lastElement));
 
-    // varijabala koja služi da se slider pomjera u lijevo za velicinu slike koja dolazi
-    const moveSliderToRightOrLeft =
-      parseInt($(sliderBox).css("left")) - lastImageWidth;
+        //pomjera slider u pocetnu poziciju kako nebi otisao lijevo van vidokruga
+        $(sliderBox).css({ left: moveSliderVar, marginRight: moveSliderVar });
+      });
+    } else if (whichButtonIsClicked === "prev") {
+      let moveSliderVar = firstImageWidth * -1;
+      // varijabala koja služi da se slider pomjera u desno za velicinu slike koja dolazi
+      let moveSliderToRightOrLeft =
+        parseInt($(sliderBox).css("left")) + firstImageWidth;
+      //pomjera slider u desno za velicinu slike koja dolazi na prvo mjesto
 
-    $(sliderBox).animate({ left: moveSliderToRightOrLeft }, 400, function () {
-      //pomjera prvu sliku u lijevo, a zadnju sliku u slideru na prvo mjesto (obrnuto zbog CCS "direction:rtl;")
-      $(firstElement).before($(lastElement));
+      $(sliderBox).animate({ left: moveSliderToRightOrLeft }, 400, function () {
+        //pomjera prvu sliku u desno, a iduca slika po redu ide na prvo mjesto (obrnuto zbog CCS "direction:rtl;")
+        $(lastElement).after($(firstElement));
 
-      //pomjera slider u pocetnu poziciju kako nebi otisao lijevo van vidokruga
-      $(sliderBox).css({ left: moveSliderVar, marginRight: moveSliderVar });
-    });
-  };
+        //pomjera slider u pocetnu poziciju kako nebi otisao lijevo van viewporta
 
-  const moveSliderToPrevious = function (
-    sliderBox,
-    sliderArray,
-    firstElement,
-    lastElement
-  ) {
-    const firstImageWidth = $(sliderArray).first().children("img").outerWidth();
-
-    const moveSliderVar = firstImageWidth * -1;
-
-    // varijabala koja služi da se slider pomjera u desno za velicinu slike koja dolazi
-    const moveSliderToRightOrLeft =
-      parseInt($(sliderBox).css("left")) + firstImageWidth;
-
-    //pomjera slider u desno za velicinu slike koja dolazi na prvo mjesto
-
-    $(sliderBox).animate({ left: moveSliderToRightOrLeft }, 400, function () {
-      //pomjera prvu sliku u desno, a iduca slika po redu ide na prvo mjesto (obrnuto zbog CCS "direction:rtl;")
-      $(lastElement).after($(firstElement));
-
-      //pomjera slider u pocetnu poziciju kako nebi otisao lijevo van viewporta
-
-      $(sliderBox).css({ left: moveSliderVar, marginRight: moveSliderVar });
-    });
+        $(sliderBox).css({ left: moveSliderVar, marginRight: moveSliderVar });
+      });
+    }
   };
 
   //Init glavne logike slidera
 
   $(".nav button").on("click", function () {
-    const whichButtonIsClicked = $(this).data("nav");
-
-    if (whichButtonIsClicked === "next") {
-      moveSliderToNext(
-        ".slider__first--ul",
-        ".slider__first--ul li",
-        ".slider__first--ul li:first",
-        ".slider__first--ul li:last"
-      );
-      moveSliderToNext(
-        ".slider__second--ul",
-        ".slider__second--ul li",
-        ".slider__second--ul li:first",
-        ".slider__second--ul li:last"
-      );
-
-      //Treci slider ...
-    } else if (whichButtonIsClicked === "prev") {
-      moveSliderToPrevious(
-        ".slider__first--ul",
-        ".slider__first--ul li",
-        ".slider__first--ul li:first",
-        ".slider__first--ul li:last"
-      );
-      moveSliderToPrevious(
-        ".slider__second--ul",
-        ".slider__second--ul li",
-        ".slider__second--ul li:first",
-        ".slider__second--ul li:last"
-      );
-      //Treci slider ...
-    }
+    let whichButtonIsClicked = $(this).data("nav");
+    moveSliderToNextOrPrevious(
+      ".slider__first--ul",
+      ".slider__first--ul li",
+      ".slider__first--ul li:first",
+      ".slider__first--ul li:last",
+      whichButtonIsClicked
+    );
+    moveSliderToNextOrPrevious(
+      ".slider__second--ul",
+      ".slider__second--ul li",
+      ".slider__second--ul li:first",
+      ".slider__second--ul li:last",
+      whichButtonIsClicked
+    );
   });
 });
